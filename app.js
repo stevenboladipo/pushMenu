35145063534a41422620cd533361032e3e662194
Steven B. Oladipo <stevenboladipo@gmail.com>
2016-11-02 19:01:25 -0400

    Basic Push Menu
    html, csss, javaScript, and jQuery

---
+++ app.js
@@ -0,0 +1,15 @@
+$('.icon-menu').click(function() {
+    $('.menu').animate({
+    left: '0px'
+  }, 200);
+  
+  $('icon-close').click(function(){ 
+      $('.menu').animate({
+          left: "-285px"
+      }, 200);
+
+  $('body').animate({
+    left: '285px'
+  }, 200);
+  });
+});
